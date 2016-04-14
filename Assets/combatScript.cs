4d692188728b4952507d68f3cf0aa1d8a85f3080
svc-student <svc-student@student.directory.jccc>
2016-04-14 16:41:20 -0500

    First Commit

---
+++ Assets/combatScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class combatScript : MonoBehaviour {
+
+    public int playerHealth;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        playerHealth = 100;
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void onTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerHealth = playerHealth - 5;
+        }
+    }
+}
