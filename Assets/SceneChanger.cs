830e4980198d53050ba55d5e9d0c179c58d648ab
svc-student <svc-student@student.directory.jccc>
2016-04-18 10:36:53 -0500

    Fixed boss Skeleton size. Made PowerCore. lighting work

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
