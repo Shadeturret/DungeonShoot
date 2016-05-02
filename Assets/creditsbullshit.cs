0f94a4572f2d1071d6f483dfc0672a99a140dcdc
svc-student <svc-student@student.directory.jccc>
2016-05-02 15:22:46 -0500

    Final (for real this time)

---
+++ Assets/creditsbullshit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditsbullshit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.visible = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = !Cursor.visible;
+        }
+	}
+
+    void Awake()
+    {
+        Cursor.visible = true;
+    }
+}
